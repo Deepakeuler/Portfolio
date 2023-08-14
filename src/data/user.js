7aa7f0a018183b713b64c7dcd5bd8b8f0f56039e
DeepakEuler <deepaksharmaforwork@gmail.com>
2023-08-14 13:26:53 +0530

    description added

--- src/data/user.js
+++ src/data/user.js
@@ -16,9 +16,9 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Backend Develper",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"Passionate about backend engineering, I thrive on devising efficient, scalable solutions that amplify user experiences. With a strong foundation in Computer Science Engineering, I embrace complex challenges with enthusiasm, leveraging my proactive problem-solving approach to craft innovative solutions. I'm dedicated to staying at the forefront of this dynamic field, continuously seeking opportunities for growth and learning. My commitment to excellence drives me to contribute meaningfully to backend engineering projects that push boundaries.",
 	},
 
 	about: {
